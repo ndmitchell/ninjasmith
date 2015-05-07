e9e117bbcb05a826d3ad2507d4af178b1fa5e848
Neil Mitchell <ndmitchell@gmail.com>
2015-05-07 18:22:35 +0100

    Add an --env support to write out environment variables

--- src/Record.hs
+++ src/Record.hs
@@ -11,13 +11,14 @@ import Data.Hashable
 import qualified Data.ByteString.Char8 as BS
 
 
-data Arg = Out FilePath | Lit String | In FilePath | DepFile FilePath | Dep String
+data Arg = Out FilePath | Lit String | Env String | In FilePath | DepFile FilePath | Dep String
 
 parseArgs :: [String] -> [Arg]
 parseArgs = f (error "Needs an argument first")
     where
         f c ("--out":xs) = f Out xs
         f c ("--lit":xs) = f Lit xs
+        f c ("--env":xs) = f Env xs
         f c ("--in":xs) = f In xs
         f c ("--depfile":xs) = f DepFile xs
         f c ("--dep":xs) = f Dep xs
@@ -30,6 +31,7 @@ main = do
         Out x -> do
             res <- fmap catMaybes $ forM xs $ \x -> case x of
                 Lit s -> return $ Just s
+                Env s -> Just <$> getEnv s
                 In s -> do src <- BS.readFile s; return $ Just $ s ++ " = " ++ show (hash src)
                 _ -> return Nothing
             writeFile x $ unlines res
